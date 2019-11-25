b089dbd8bf8b24ddbc10f8d58009c5d9bedd539d
Hasan Kemal Demirci <ha_rap@hotmail.com>
2019-11-25 11:14:59 +0300

    Creates POST method to /tasksendpoint

--- src/app.js
+++ src/app.js
@@ -2,4 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
+app.post("/tasks", (req, res) => {
+  res.status(201).send(req.body);
+});
+
 app.listen(port, () => console.log(`App is listening on port ${port}!`));
