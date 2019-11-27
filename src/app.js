d645275fe67d5d963cdbc7a265a1207672627ec0
canyener <can.yener@hotmail.com.tr>
2019-11-27 20:03:20 +0300

    Recaps what we have done

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-const express = require("express")
+const express = require('express')
+
+require('./db/mongoose')
+
+const taskRouter = require('./routers/task')
+
 const app = express()
+
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
-require("./db/mongoose")
-
-// Routers
-const tasks = require("./routers/tasks")
-
-app.use("/tasks", tasks)
+app.use('/tasks', taskRouter)
 
 app.listen(port, () => console.log(`App is listening on port ${port}!`))
