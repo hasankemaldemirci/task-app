8b11eac02b320bfa6576c3d40eb668a33441044a
Hasan Kemal Demirci <ha_rap@hotmail.com>
2019-11-27 12:11:36 +0300

    Saves task to database

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 const express = require("express")
+
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post("/tasks", (req, res) => {
-  res.status(201).send(req.body)
+require("./db/mongoose")
+
+const Task = require("./models/task")
+
+app.post("/tasks", async (req, res) => {
+  try {
+    const task = await new Task(req.body).save()
+    res.status(201).send(task)
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
 app.listen(port, () => console.log(`App is listening on port ${port}!`))
