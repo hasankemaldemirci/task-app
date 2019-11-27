d43360f8ad86c0381e4ce0297529e374d36943b0
Hasan Kemal Demirci <ha_rap@hotmail.com>
2019-11-27 17:32:49 +0300

    Adds tasks router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require("express")
-
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -7,34 +6,9 @@ app.use(express.json())
 
 require("./db/mongoose")
 
-const Task = require("./models/task")
+// Routers
+const tasks = require("./routers/tasks")
 
-app.post("/tasks", async (req, res) => {
-  try {
-    const task = new Task(req.body)
-    await task.save()
-    res.status(201).send(task)
-  } catch (err) {
-    res.status(400).send(err)
-  }
-})
-
-app.get("/tasks", async (req, res) => {
-  try {
-    const tasks = await Task.find({})
-    res.status(200).send(tasks)
-  } catch (err) {
-    res.status(500)
-  }
-})
-
-app.get("/tasks/:id", async (req, res) => {
-  try {
-    const task = await Task.findById(req.params.id)
-    res.status(200).send(task)
-  } catch (err) {
-    res.status(500)
-  }
-})
+app.use("/tasks", tasks)
 
 app.listen(port, () => console.log(`App is listening on port ${port}!`))
