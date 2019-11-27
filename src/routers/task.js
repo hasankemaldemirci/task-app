c5c615f5f639d20405b766b235e72a3b27ad0c3e
canyener <can.yener@hotmail.com.tr>
2019-11-27 20:03:48 +0300

    Renames task router

---
+++ src/routers/task.js
@@ -0,0 +1,37 @@
+const express = require('express')
+
+const Task = require('../models/task')
+
+const router = new express.Router()
+
+router.post('/', async (req, res) => {
+  try {
+    const task = new Task(req.body)
+    await task.save()
+    res.status(201).send(task)
+  } catch (err) {
+    res.status(400).send(err)
+  }
+})
+
+router.get('/', async (req, res) => {
+  try {
+    const tasks = await Task.find({})
+    res.status(200).send(tasks)
+  } catch (err) {
+    res.status(500).send()
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id)
+    res.status(200).send(task)
+  } catch (err) {
+    res.status(500).send()    
+  }
+})
+
+module.exports = router
+
+
