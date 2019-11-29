7fdfcdd00176b01cafd751bce917765a9ac87751
Hasan Kemal Demirci <ha_rap@hotmail.com>
2019-11-29 10:35:33 +0300

    Fixes document format

--- src/models/task.js
+++ src/models/task.js
@@ -1,20 +1,20 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
 const taskSchema = new mongoose.Schema({
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
+	description: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	completed: {
+		type: Boolean,
+		default: false
 	}
 },{
 	timestamps: true,
 	versionKey: false
 })
 
-const Task = mongoose.model("Task", taskSchema)
+const Task = mongoose.model('Task', taskSchema)
 
 module.exports = Task
