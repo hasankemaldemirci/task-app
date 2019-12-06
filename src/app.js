244c0f772d2b5b3a055a1839a77902797669895b
Hasan Kemal Demirci <ha_rap@hotmail.com>
2019-12-06 11:00:15 +0300

    Seperates dev & test enviroments and modify scripts

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const taskRouter = require('./routers/task')
 
 const app = express()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.use(express.json())
 app.use('/tasks', taskRouter)
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost:27017/task-api-kata", {
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
