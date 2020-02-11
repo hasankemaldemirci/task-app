00fc2a87a79f351393581bb98f2e03c104b6db75
Hasan Kemal Demirci <ha_rap@hotmail.com>
2020-02-11 17:32:36 +0300

    Adds 'should return 201' test case and create post user router

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,13 +1,13 @@
 const express = require('express')
 
-const router = new express.Router()
-
 const User = require('../models/user')
+
+const router = new express.Router()
 
 router.post('/', async (req, res) => {
   const user = new User(req.body)
   await user.save()
-  res.status(201).send(user)
+  res.status(201).send()
 })
 
 module.exports = router
