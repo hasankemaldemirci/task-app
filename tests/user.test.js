46befe19dbe1fddf86da0a77e5fa287384f53ee0
Hasan Kemal Demirci <ha_rap@hotmail.com>
2020-02-12 10:47:13 +0300

    Adds 'Should save valid user to database' test case

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,6 +2,8 @@ const request = require('supertest')
 
 const app = require('../src/app')
 
+const User = require('../src/models/user')
+
 const { setupDatabase } = require('./fixtures/db')
 
 beforeEach(setupDatabase)
@@ -32,5 +34,21 @@ describe('POST /users', () => {
       .send(validUser)
 
     expect(response.body).toMatchObject(validUser)
+  })
+
+  test('Should save valid user to database', async () => {
+    const validUser = {
+      name: 'Hasan',
+      email: 'test@test.com',
+      password: '1234567'
+    }
+
+    await request(app)
+      .post('/users')
+      .send(validUser)
+
+    const user = User.findOne(validUser.email)
+
+    expect(user).toBeTruthy()
   })
 })
