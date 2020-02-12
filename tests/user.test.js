c9380f58a27a8b3091c28772828cd1678035aaee
Hasan Kemal Demirci <ha_rap@hotmail.com>
2020-02-12 14:41:51 +0300

    Adds 'Should trim user name before saving' test case

--- tests/user.test.js
+++ tests/user.test.js
@@ -67,4 +67,20 @@ describe('POST /users', () => {
 
     expect(response.body.message).toEqual(expectedErrorMessage)
   })
+
+  test('Should trim user name before saving', async () => {
+    const validUser = {
+      name: '   Hasan   ',
+      email: 'test@test.com',
+      password: '1234567'
+    }
+
+    await request(app)
+      .post('/users')
+      .send(validUser)
+
+    const user = await User.findOne({ email: validUser.email })
+
+    expect(user.name).toEqual('Hasan')
+  })
 })
