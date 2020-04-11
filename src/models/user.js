1657cdf95771f458d2ec62cf47302d91f22aaac6
Hasan Kemal Demirci <ha_rap@hotmail.com>
2020-04-11 22:57:53 +0300

    Deletes password field from created user

--- src/models/user.js
+++ src/models/user.js
@@ -57,6 +57,15 @@ userSchema.pre('save', async function (next) {
   next()
 })
 
+userSchema.methods.toJSON = function () {
+  const user = this
+  const userObject = user.toObject()
+
+  delete userObject.password
+
+  return userObject
+}
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = User
